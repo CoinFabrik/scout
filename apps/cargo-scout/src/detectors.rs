944b73c6e09b107c00c73c4d0ca431c9b28076bd
Jose Garcia <josegarcia@MacBook-Pro-de-Jose.local>
2023-06-05 11:09:41 -0300

    Add filtering, excluding and listing functions to Detectors

--- apps/cargo-scout/src/detectors.rs
+++ apps/cargo-scout/src/detectors.rs
@@ -1,6 +1,6 @@
 use std::{path::PathBuf, rc::Rc};
 
-use anyhow::{Ok, Result};
+use anyhow::{bail, Ok, Result};
 use cargo::Config;
 use itertools::Itertools;
 
@@ -78,5 +78,69 @@ impl Detectors {
         let builder =
             DetectorBuilder::new(&self.cargo_config, detectors_config, self.metadata.clone());
         builder.build(used_detectors)
+    }
+
+    pub fn get_filtered_detectors(
+        filter: String,
+        detectors_names: Vec<String>,
+    ) -> Result<Vec<String>> {
+        let mut used_detectors: Vec<String> = Vec::new();
+        let parsed_detectors = filter
+            .to_lowercase()
+            .trim()
+            .replace('_', "-")
+            .split(',')
+            .map(|detector| detector.trim().to_string())
+            .collect::<Vec<String>>();
+        for detector in parsed_detectors {
+            if detectors_names.contains(&detector.to_string()) {
+                used_detectors.push(detector.to_string());
+            } else {
+                bail!("The detector '{}' doesn't exist", detector);
+            }
+        }
+        Ok(used_detectors)
+    }
+
+    pub fn get_excluded_detectors(
+        excluded: String,
+        detectors_names: Vec<String>,
+    ) -> Result<Vec<String>> {
+        let mut used_detectors = detectors_names.clone();
+        let parsed_detectors = excluded
+            .to_lowercase()
+            .trim()
+            .replace('_', "-")
+            .split(',')
+            .map(|detector| detector.trim().to_string())
+            .collect::<Vec<String>>();
+        for detector in parsed_detectors {
+            if detectors_names.contains(&detector.to_string()) {
+                let index = used_detectors.iter().position(|x| x == &detector).unwrap();
+                used_detectors.remove(index);
+            } else {
+                bail!("The detector '{}' doesn't exist", detector);
+            }
+        }
+        Ok(used_detectors)
+    }
+
+    pub fn list_detectors(detectors_names: Vec<String>) -> Result<()> {
+        let separator = "─".repeat(48);
+        let upper_border = format!("┌{}┐", separator);
+        let lower_border = format!("└{}┘", separator);
+        let empty_line = format!("│{:48}│", "");
+
+        println!("{}", upper_border);
+        println!("│{:^48}│", "Available detectors:");
+        println!("{}", empty_line);
+
+        for (index, detector_name) in detectors_names.iter().enumerate() {
+            println!("│ {:<1}. {:<44}│", index + 1, detector_name);
+        }
+
+        println!("{}", empty_line);
+        println!("{}", lower_border);
+        Ok(())
     }
 }
