da8df791e725d4a13b3e9423621cf8604569e300
Facundo Lerena <faculerena@gmail.com>
2023-06-13 10:32:00 -0300

    Fixed cargo.toml to make the tests and detector documentation

--- detectors/delegate-call/src/lib.rs
+++ detectors/delegate-call/src/lib.rs
@@ -18,20 +18,45 @@ use rustc_span::Span;
 
 dylint_linting::declare_late_lint! {
     /// ### What it does
-    ///
+    /// Checks for delegated calls to contracts passed as arguments.
     /// ### Why is this bad?
-    ///
+    ///Delegated calls to contracts passed as arguments can be used to change the expected behavior of the contract. If you need to change the target of a delegated call, you should use a storage variable, and make a function with proper access control to change it.
     /// ### Known problems
     /// Remove if none.
     ///
     /// ### Example
     /// ```rust
-    /// // example code where a warning is issued
-    /// ```
+    ///pub fn delegateCall(&mut self, target: Hash, argument: Balance) {
+    ///    let selector_bytes = [0x0, 0x0, 0x0, 0x0];
+    ///    let result: T  = build_call::<DefaultEnvironment>()
+    ///        .delegate(target)
+    ///        .exec_input(
+    ///            ExecutionInput::new(Selector::new(selector_bytes))
+    ///                .push_arg(argument)
+    ///     )
+    ///        .returns::<T>()
+    ///     .invoke();
+    ///}
+    ///     ```
     /// Use instead:
-    /// ```rust
-    /// // example code that does not raise a warning
-    /// ```
+    ///```rust
+    ///pub fn delegateCall(&mut self, argument: Balance) {
+    ///    let selector_bytes = [0x0, 0x0, 0x0, 0x0];
+    ///    let result: T  = build_call::<DefaultEnvironment>()
+    ///       .delegate(self.target)
+    ///        .exec_input(
+    ///            ExecutionInput::new(Selector::new(selector_bytes))
+    ///                .push_arg(argument)
+    ///        )
+    ///        .returns::<T>()
+    ///        .invoke();
+    ///}
+    ///
+    ///pub fn set_target(&mut self, new_target: Hash) {
+    ///    assert_eq!(self.admin, self.env().caller(), "Only admin can set target");
+    ///   self.target = new_target;
+    ///}
+    ///```
     pub DELEGATE_CALL,
     Warn,
     "description goes here"
