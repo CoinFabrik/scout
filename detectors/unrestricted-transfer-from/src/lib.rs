f881a1b8ae1a49c70a355ca0cdcf1e2d20e60a25
Jose Garcia Crosta <jgcrosta@gmail.com>
2023-06-29 17:26:18 -0300

    Fix clippy errors on detector

--- detectors/unrestricted-transfer-from/src/lib.rs
+++ detectors/unrestricted-transfer-from/src/lib.rs
@@ -1,22 +1,22 @@
 #![feature(rustc_private)]
-#![warn(unused_extern_crates)]
 #![feature(let_chains)]
 
+extern crate rustc_ast;
 extern crate rustc_hir;
 extern crate rustc_middle;
 extern crate rustc_span;
-extern crate rustc_ast;
 
-use rustc_ast::ast::UintTy;
-use rustc_ast::{LitKind, LitIntType};
-use rustc_hir::{FnRetTy, Expr, ExprKind, intravisit::{walk_expr, Visitor}};
-use rustc_lint::{LateContext, LateLintPass};
-use rustc_middle::mir::{Place, BasicBlocks};
-use rustc_middle::mir::{BasicBlock, StatementKind, Local, TerminatorKind, Operand, interpret::{ConstValue}};
-use rustc_span::Span;
-use rustc_span::def_id::DefId;
-use rustc_span::sym::Default;
 use clippy_utils::diagnostics::span_lint;
+use rustc_ast::ast::UintTy;
+use rustc_ast::{LitIntType, LitKind};
+use rustc_hir::{
+    intravisit::{walk_expr, Visitor},
+    Expr, ExprKind, FnRetTy,
+};
+use rustc_lint::{LateContext, LateLintPass};
+use rustc_middle::mir::BasicBlocks;
+use rustc_middle::mir::{BasicBlock, Local, Operand, StatementKind, TerminatorKind};
+use rustc_span::Span;
 
 dylint_linting::impl_late_lint! {
     pub UNRESTRICTED_TRANSFER_FROM,
@@ -26,36 +26,33 @@ dylint_linting::impl_late_lint! {
 }
 
 #[derive(Default)]
-pub struct UnrestrictedTransferFrom {
-}
+pub struct UnrestrictedTransferFrom {}
 impl UnrestrictedTransferFrom {
     pub fn new() -> Self {
-        Self {
-        }
+        Self {}
     }
 }
 
 impl<'tcx> LateLintPass<'tcx> for UnrestrictedTransferFrom {
-    
     fn check_fn(
         &mut self,
         cx: &LateContext<'tcx>,
-        _:rustc_hir::intravisit::FnKind<'tcx>,
+        _: rustc_hir::intravisit::FnKind<'tcx>,
         fn_decl: &'tcx rustc_hir::FnDecl<'tcx>,
         body: &'tcx rustc_hir::Body<'tcx>,
-        _:Span,
-        localdef:rustc_span::def_id::LocalDefId) {
-        
-        struct SetAllowReentrancyFinder<'tcx,'tcx_ref>{
+        _: Span,
+        localdef: rustc_span::def_id::LocalDefId,
+    ) {
+        struct SetAllowReentrancyFinder<'tcx, 'tcx_ref> {
             cx: &'tcx_ref LateContext<'tcx>,
             def_id: Option<rustc_span::def_id::DefId>,
             pusharg_def_id: Option<rustc_span::def_id::DefId>,
         }
 
         let mut sarf = SetAllowReentrancyFinder {
-            cx: cx,
+            cx,
             def_id: None,
-            pusharg_def_id: None
+            pusharg_def_id: None,
         };
 
         impl<'tcx> Visitor<'tcx> for SetAllowReentrancyFinder<'tcx, '_> {
@@ -63,20 +60,20 @@ impl<'tcx> LateLintPass<'tcx> for UnrestrictedTransferFrom {
                 match expr.kind {
                     ExprKind::Call(path, args) => {
                         // Look for Selector::new([0x54, 0xb3, 0xc7, 0x6e])
-                        if let ExprKind::Path(qpath) = &path.kind && 
+                        if let ExprKind::Path(qpath) = &path.kind &&
                             let rustc_hir::QPath::TypeRelative(ty, path_segment) = qpath &&
                             path_segment.ident.name.to_string() == "new" &&
-                            let rustc_hir::TyKind::Path(qpath_2) = &ty.kind && 
-                            let rustc_hir::QPath::Resolved(ty_2, path_2) = qpath_2 && 
-                            path_2.segments.into_iter().any(|s|s.ident.name.to_string() == "Selector") &&
+                            let rustc_hir::TyKind::Path(qpath_2) = &ty.kind &&
+                            let rustc_hir::QPath::Resolved(_, path_2) = qpath_2 &&
+                            path_2.segments.iter().any(|s|s.ident.name.to_string() == "Selector") &&
                             args.len() == 1 &&
                             let ExprKind::Array(sel_arr) = args.first().unwrap().kind &&
                             sel_arr.len() == 4 {
                                 let transfer_from_selector = [0x54, 0xb3, 0xc7, 0x6e];
                                 let mut is_tranfer_from = true;
 
-                                for (id, expr) in sel_arr.into_iter().enumerate() {
-                                    if let ExprKind::Lit(byte) = expr.kind && 
+                                for (id, expr) in sel_arr.iter().enumerate() {
+                                    if let ExprKind::Lit(byte) = expr.kind &&
                                         let LitKind::Int(value, int_ty) = byte.node &&
                                         let LitIntType::Unsigned(u_ty) = int_ty &&
                                         u_ty == UintTy::U8 &&
@@ -89,27 +86,28 @@ impl<'tcx> LateLintPass<'tcx> for UnrestrictedTransferFrom {
                                     self.def_id = self.cx.typeck_results().type_dependent_def_id(path.hir_id);
                                 }
                         }
-                    },
-                    ExprKind::MethodCall(path, self_arg, ..) => {
+                    }
+                    ExprKind::MethodCall(path, ..) => {
                         if path.ident.name.to_string() == "push_arg" {
-                            self.pusharg_def_id = self.cx.typeck_results().type_dependent_def_id(expr.hir_id);
+                            self.pusharg_def_id =
+                                self.cx.typeck_results().type_dependent_def_id(expr.hir_id);
                         }
-                    },
-                    _=>{}
+                    }
+                    _ => {}
                 }
-        
+
                 walk_expr(self, expr);
             }
         }
 
         if let FnRetTy::Return(ret_ty) = fn_decl.output &&
             let rustc_hir::TyKind::Path(qpath) = &ret_ty.kind &&
-            let rustc_hir::QPath::Resolved(_, path) = qpath && 
+            let rustc_hir::QPath::Resolved(_, path) = qpath &&
             path.segments.last()
-                .map_or(false, |s| 
+                .map_or(false, |s|
                     s.ident.name.to_string() == "CallBuilder" ||
                     s.ident.name.to_string() == "CreateBuilder"
-            ) {    
+            ) {
             return;
         }
 
@@ -125,67 +123,70 @@ impl<'tcx> LateLintPass<'tcx> for UnrestrictedTransferFrom {
 
         for bb in mir_body.basic_blocks.iter() {
             for statement in &bb.statements {
-                match &statement.kind {
-                    StatementKind::Assign(assign) => {
-                        match &assign.1 {
-                            rustc_middle::mir::Rvalue::Ref(_, _, origplace) |
-                            rustc_middle::mir::Rvalue::AddressOf(_, origplace) |
-                            rustc_middle::mir::Rvalue::Len(origplace) |
-                            rustc_middle::mir::Rvalue::CopyForDeref(origplace) => {
-                                if tainted_locals.clone().into_iter().any(|local|local == origplace.local) {
+                if let StatementKind::Assign(assign) = &statement.kind {
+                    match &assign.1 {
+                        rustc_middle::mir::Rvalue::Ref(_, _, origplace)
+                        | rustc_middle::mir::Rvalue::AddressOf(_, origplace)
+                        | rustc_middle::mir::Rvalue::Len(origplace)
+                        | rustc_middle::mir::Rvalue::CopyForDeref(origplace) => {
+                            if tainted_locals
+                                .clone()
+                                .into_iter()
+                                .any(|local| local == origplace.local)
+                            {
+                                tainted_locals.push(assign.0.local);
+                            }
+                        }
+                        rustc_middle::mir::Rvalue::Use(operand) => match &operand {
+                            Operand::Copy(origplace) | Operand::Move(origplace) => {
+                                if tainted_locals
+                                    .clone()
+                                    .into_iter()
+                                    .any(|local| local == origplace.local)
+                                {
                                     tainted_locals.push(assign.0.local);
                                 }
-                            },
-                            rustc_middle::mir::Rvalue::Use(operand) => {
-                                match &operand {
-                                    Operand::Copy(origplace) |
-                                    Operand::Move(origplace) => {
-                                        if tainted_locals.clone().into_iter().any(|local|local == origplace.local) {
-                                            tainted_locals.push(assign.0.local);
-                                        }
-                                    },
-                                    _ =>{}
-                                }
-                            },
+                            }
                             _ => {}
-                        }
-                    },
-                    _ => {}
+                        },
+                        _ => {}
+                    }
                 }
             }
         }
         for bb in mir_body.basic_blocks.iter() {
             if let TerminatorKind::Call {
                 func,
-                args,
+                args: _,
                 destination,
                 target,
-                unwind,
-                from_hir_call,
-                fn_span 
-            } = &bb.terminator().kind {
+                unwind: _,
+                from_hir_call: _,
+                fn_span: _,
+            } = &bb.terminator().kind
+            {
                 if let Operand::Constant(cont) = func &&
-                    let rustc_middle::mir::ConstantKind::Val(const_val, val_type) = &cont.literal &&
-                    let rustc_middle::ty::TyKind::FnDef(def, subs) = val_type.kind() &&
+                    let rustc_middle::mir::ConstantKind::Val(_, val_type) = &cont.literal &&
+                    let rustc_middle::ty::TyKind::FnDef(def, _) = val_type.kind() &&
                     sarf.def_id.is_some_and(|id|id==*def) &&
                     target.is_some() {
                         //aca el terminator es la llamada a new, el destination tiene el place con el selector
                         //a partir de aca lo que hago es buscar donde se usa el selector y donde se le pushean args dados por el usuario
                         let mut tainted_selector_places: Vec<Local> = vec![destination.local];
-                        fn navigate_trough_bbs(cx: &LateContext, bb: &BasicBlock, bbs: &BasicBlocks, tainted_locals: &Vec<Local>, tainted_selector_places: &mut Vec<Local>, sarf: &SetAllowReentrancyFinder) {
+                        fn navigate_trough_bbs(cx: &LateContext, bb: &BasicBlock, bbs: &BasicBlocks, tainted_locals: &Vec<Local>, _tainted_selector_places: &mut Vec<Local>, sarf: &SetAllowReentrancyFinder) {
                             if let TerminatorKind::Call {
                                     func,
                                     args,
-                                    destination,
+                                    destination: _,
                                     target,
-                                    unwind,
-                                    from_hir_call,
+                                    unwind: _,
+                                    from_hir_call: _,
                                     fn_span
                                 } = &bbs[*bb].terminator().kind &&
                                 let Operand::Constant(cont) = func &&
-                                let rustc_middle::mir::ConstantKind::Val(const_val, val_type) = &cont.literal &&
-                                let rustc_middle::ty::TyKind::FnDef(def, subs) = val_type.kind() {
-                                
+                                let rustc_middle::mir::ConstantKind::Val(_, val_type) = &cont.literal &&
+                                let rustc_middle::ty::TyKind::FnDef(def, _) = val_type.kind() {
+
                                 if sarf.pusharg_def_id.is_some_and(|id|id==*def) {
                                     for arg in args {
                                         if arg.place().map_or(false, |place|tainted_locals.iter().any(|l|l == &place.local)) {
@@ -194,7 +195,7 @@ impl<'tcx> LateLintPass<'tcx> for UnrestrictedTransferFrom {
                                     }
                                 }
                                 if target.is_some() {
-                                    navigate_trough_bbs(cx,&target.unwrap(), bbs, tainted_locals, tainted_selector_places, sarf);
+                                    navigate_trough_bbs(cx,&target.unwrap(), bbs, tainted_locals, _tainted_selector_places, sarf);
                                 }
                             }
                         }
@@ -202,43 +203,5 @@ impl<'tcx> LateLintPass<'tcx> for UnrestrictedTransferFrom {
                 }
             }
         }
-
-        /*
-        let mut allow_reentry_true_places: Vec<&Place> = vec![];
-        for bb in mir_body.basic_blocks.into_iter() {
-            if let TerminatorKind::Call {
-                func,
-                args,
-                destination,
-                target,
-                unwind,
-                from_hir_call,
-                fn_span 
-            } = &bb.terminator().kind {
-                if let Operand::Constant(cont) = func {
-                    if let rustc_middle::mir::ConstantKind::Val(const_val, val_type) = &cont.literal {
-                        if let rustc_middle::ty::TyKind::FnDef(def, subs) = val_type.kind(){
-                            if sarf.def_id.is_some_and(|id|id==*def) {
-                                //Check that second argument is true in set_allow_reentry [set_allow_reentry(self, true)]
-                                if args.last().is_some() && 
-                                    let Operand::Constant(arg) = args.last().unwrap() &&
-                                    let ConstantKind::Val(const_val, val_type) = arg.literal &&
-                                    *val_type.kind() == TyKind::Bool &&
-                                    let ConstValue::Scalar(scalar) = const_val &&
-                                    scalar.to_bool().map_or_else(|e| false, |f| f) {
-                                        navigateTroughBasicBlocks(&target.unwrap(), &mir_body.basic_blocks, &vec![destination], sarf.invoke_def_id.unwrap(), cx)
-                                }
-                            }
-                        }
-                    }
-                } else if let Operand::Copy(cpy) = func {
-                    dbg!(cpy);
-                } else if let Operand::Move(mov) = func {
-                    dbg!(mov);
-                }
-            }
-        }
-        */
-        
     }
 }
