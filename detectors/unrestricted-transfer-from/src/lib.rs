293bbf1aad645d13446cbe621130052c22ef0f8e
Facundo Lerena <faculerena@gmail.com>
2023-07-13 15:11:26 -0300

    Added detector for ContractRef cross contract call

--- detectors/unrestricted-transfer-from/src/lib.rs
+++ detectors/unrestricted-transfer-from/src/lib.rs
@@ -7,15 +7,18 @@ extern crate rustc_middle;
 extern crate rustc_span;
 
 use clippy_utils::diagnostics::span_lint;
-use rustc_ast::ast::UintTy;
-use rustc_ast::{LitIntType, LitKind};
+use if_chain::if_chain;
+use rustc_ast::{ast::UintTy, LitIntType, LitKind};
 use rustc_hir::{
+    Body,
     intravisit::{walk_expr, Visitor},
     Expr, ExprKind, FnRetTy,
 };
+use rustc_hir::def::Res;
+use rustc_hir::{ PatKind, QPath};
+
 use rustc_lint::{LateContext, LateLintPass};
-use rustc_middle::mir::BasicBlocks;
-use rustc_middle::mir::{BasicBlock, Local, Operand, StatementKind, TerminatorKind};
+use rustc_middle::mir::{BasicBlock, BasicBlocks, Local, Operand, StatementKind, TerminatorKind};
 use rustc_span::Span;
 
 dylint_linting::impl_late_lint! {
@@ -43,19 +46,18 @@ impl<'tcx> LateLintPass<'tcx> for UnrestrictedTransferFrom {
         _: Span,
         localdef: rustc_span::def_id::LocalDefId,
     ) {
-        struct SetAllowReentrancyFinder<'tcx, 'tcx_ref> {
+        struct UnrestrictedTransferFromFinder<'tcx, 'tcx_ref> {
             cx: &'tcx_ref LateContext<'tcx>,
             def_id: Option<rustc_span::def_id::DefId>,
             pusharg_def_id: Option<rustc_span::def_id::DefId>,
+            span: Option<Span>,
+            from_ref: bool,
+            the_body: &'tcx Body<'tcx>,
+
         }
 
-        let mut sarf = SetAllowReentrancyFinder {
-            cx,
-            def_id: None,
-            pusharg_def_id: None,
-        };
 
-        impl<'tcx> Visitor<'tcx> for SetAllowReentrancyFinder<'tcx, '_> {
+        impl<'tcx> Visitor<'tcx> for UnrestrictedTransferFromFinder<'tcx, '_> {
             fn visit_expr(&mut self, expr: &'tcx Expr<'_>) {
                 match expr.kind {
                     ExprKind::Call(path, args) => {
@@ -86,6 +88,57 @@ impl<'tcx> LateLintPass<'tcx> for UnrestrictedTransferFrom {
                                     self.def_id = self.cx.typeck_results().type_dependent_def_id(path.hir_id);
                                 }
                         }
+
+
+                        if_chain! {
+                            if let ExprKind::Path(qpath) = &path.kind;
+                            if let rustc_hir::QPath::TypeRelative(ty, path_segment) = qpath;
+                            if path_segment.ident.name.to_string() == "transfer_from";
+                            if let rustc_hir::TyKind::Path(qpath_2) = &ty.kind;
+                            if let rustc_hir::QPath::Resolved(_, path_2) = qpath_2;
+                            if path_2.segments.iter().any(|s|s.ident.name.to_string() == "PSP22Ref");
+                            if args.len() == 5;
+                            then {
+                                let mut param_hir_ids = Vec::new();
+                                let mut arg_hir_ids = Vec::new();
+
+                                for i in 0..self.the_body.params.len() {
+                                    if let PatKind::Binding(_, hir_id, _, _) = self.the_body.params[i].pat.kind {
+                                        param_hir_ids.push(hir_id);
+                                    }
+                                }
+
+                                arg_hir_ids.push(args[1].hir_id);
+
+                                if let ExprKind::Path(qpath) = &args[1].kind {
+                                    match qpath {
+                                        QPath::Resolved(_, path) => {
+                                            if let Res::Local(hir_id) = path.res {
+                                                arg_hir_ids.push(hir_id);
+                                            }
+                                            for j in 0..path.segments.len() {
+                                                arg_hir_ids.push(path.segments[j].hir_id);
+                                            }
+                                        }
+                                        QPath::LangItem(_, _, Some(lang_item_hir_id)) => {
+                                            arg_hir_ids.push(*lang_item_hir_id);
+                                        }
+                                        _ => (),
+                                    }
+                                }
+
+
+                                for param_id in param_hir_ids {
+                                    if arg_hir_ids.contains(&param_id) {
+                                        self.span = Some(args[1].span);
+                                        self.from_ref = true;
+
+                                    }
+                                }
+                            }
+
+                        }
+
                     }
                     ExprKind::MethodCall(path, ..) => {
                         if path.ident.name.to_string() == "push_arg" {
@@ -100,6 +153,16 @@ impl<'tcx> LateLintPass<'tcx> for UnrestrictedTransferFrom {
             }
         }
 
+
+        let mut utf_storage = UnrestrictedTransferFromFinder {
+            cx,
+            def_id: None,
+            pusharg_def_id: None,
+            span: None,
+            from_ref: false,
+            the_body: body,
+        };
+
         if let FnRetTy::Return(ret_ty) = fn_decl.output &&
             let rustc_hir::TyKind::Path(qpath) = &ret_ty.kind &&
             let rustc_hir::QPath::Resolved(_, path) = qpath &&
@@ -113,9 +176,14 @@ impl<'tcx> LateLintPass<'tcx> for UnrestrictedTransferFrom {
 
         let mir_body = cx.tcx.optimized_mir(localdef.to_def_id());
 
-        walk_expr(&mut sarf, body.value);
+        walk_expr(&mut utf_storage, body.value);
 
-        if sarf.def_id.is_none() {
+        if utf_storage.from_ref {
+            span_lint(cx, UNRESTRICTED_TRANSFER_FROM, utf_storage.span.unwrap(), "this argument comes from a user-supplied argument");
+
+        }
+
+        if utf_storage.def_id.is_none() {
             return;
         }
         //vector with function args and variables derived from those args
@@ -168,12 +236,12 @@ impl<'tcx> LateLintPass<'tcx> for UnrestrictedTransferFrom {
                 if let Operand::Constant(cont) = func &&
                     let rustc_middle::mir::ConstantKind::Val(_, val_type) = &cont.literal &&
                     let rustc_middle::ty::TyKind::FnDef(def, _) = val_type.kind() &&
-                    sarf.def_id.is_some_and(|id|id==*def) &&
+                    utf_storage.def_id.is_some_and(|id|id==*def) &&
                     target.is_some() {
                         //aca el terminator es la llamada a new, el destination tiene el place con el selector
                         //a partir de aca lo que hago es buscar donde se usa el selector y donde se le pushean args dados por el usuario
                         let mut tainted_selector_places: Vec<Local> = vec![destination.local];
-                        fn navigate_trough_bbs(cx: &LateContext, bb: &BasicBlock, bbs: &BasicBlocks, tainted_locals: &Vec<Local>, _tainted_selector_places: &mut Vec<Local>, sarf: &SetAllowReentrancyFinder) {
+                        fn navigate_trough_bbs(cx: &LateContext, bb: &BasicBlock, bbs: &BasicBlocks, tainted_locals: &Vec<Local>, _tainted_selector_places: &mut Vec<Local>, utf_storage: &UnrestrictedTransferFromFinder) {
                             if let TerminatorKind::Call {
                                     func,
                                     args,
@@ -187,7 +255,7 @@ impl<'tcx> LateLintPass<'tcx> for UnrestrictedTransferFrom {
                                 let rustc_middle::mir::ConstantKind::Val(_, val_type) = &cont.literal &&
                                 let rustc_middle::ty::TyKind::FnDef(def, _) = val_type.kind() {
 
-                                if sarf.pusharg_def_id.is_some_and(|id|id==*def) {
+                                if utf_storage.pusharg_def_id.is_some_and(|id|id==*def) {
                                     for arg in args {
                                         if arg.place().map_or(false, |place|tainted_locals.iter().any(|l|l == &place.local)) {
                                             span_lint(cx, UNRESTRICTED_TRANSFER_FROM, *fn_span, "this argument comes from a user-supplied argument");
@@ -195,13 +263,14 @@ impl<'tcx> LateLintPass<'tcx> for UnrestrictedTransferFrom {
                                     }
                                 }
                                 if target.is_some() {
-                                    navigate_trough_bbs(cx,&target.unwrap(), bbs, tainted_locals, _tainted_selector_places, sarf);
+                                    navigate_trough_bbs(cx,&target.unwrap(), bbs, tainted_locals, _tainted_selector_places, utf_storage);
                                 }
                             }
                         }
-                        navigate_trough_bbs(cx,&target.unwrap(),&mir_body.basic_blocks, &tainted_locals, &mut tainted_selector_places, &sarf);
+                        navigate_trough_bbs(cx,&target.unwrap(),&mir_body.basic_blocks, &tainted_locals, &mut tainted_selector_places, &utf_storage);
                 }
             }
         }
+
     }
 }
