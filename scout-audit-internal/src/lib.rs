c5043ef1444620d6ab9970e46d0e31f03cdaef1b
Facundo Lerena <faculerena@gmail.com>
2023-09-06 17:27:27 -0300

    Added lint_helper to wrap `span_lint_and_help`

--- scout-audit-internal/src/lib.rs
+++ scout-audit-internal/src/lib.rs
@@ -1,10 +1,16 @@
+#![feature(rustc_private)]
 //! # Scout Audit Internal
 //!
 //! This library is for internal usage only by [`cargo_scout_audit`](https://crates.io/crates/cargo-scout-audit)
 #[cfg(feature = "detector")]
 mod detector;
 
+#[cfg(feature = "lint_helper")]
+mod lint_helper;
+
 #[cfg(feature = "detector")]
 pub use detector::Detector;
+#[cfg(feature = "lint_helper")]
+pub use lint_helper::span_lint_and_help;
 #[cfg(feature = "detector")]
 pub use strum::IntoEnumIterator;
