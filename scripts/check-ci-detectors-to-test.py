be3a1c2d1aad47a12e040d19bb81fcfeaf8d0e82
aon <21188659+aon@users.noreply.github.com>
2023-09-08 15:35:52 -0300

    Fix check-ci-detectors script

--- scripts/check-ci-detectors-to-test.py
+++ scripts/check-ci-detectors-to-test.py
@@ -2,6 +2,9 @@ import argparse
 import os
 import yaml
 
+def is_special_directory(directory):
+    return directory == ".cargo" or directory == ".git" or directory == "target"
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Check if all detectors were tested by the CI")
     parser.add_argument("gh_workflow_path", type=str, help="Path to the github workflow.")
@@ -12,7 +15,7 @@ if __name__ == "__main__":
         workflow = yaml.safe_load(f)
         detectors_to_test = workflow["jobs"]["test"]["strategy"]["matrix"]["test"]
 
-        detectors = [ f.name for f in os.scandir(args.detectors_path) if f.is_dir() ]
+        detectors = [ f.name for f in os.scandir(args.detectors_path) if f.is_dir() and not is_special_directory(f.name) ]
         detectors.sort()
 
         if (detectors != detectors_to_test):
