1a24408fef2209fe1a43b7ce15d35193987a5a0d
aon <21188659+aon@users.noreply.github.com>
2023-09-08 15:02:53 -0300

    Remove unused code from detectors

--- detectors/unused-return-enum/src/lib.rs
+++ detectors/unused-return-enum/src/lib.rs
@@ -177,11 +177,3 @@ impl EarlyLintPass for UnusedReturnEnum {
         }
     }
 }
-
-#[test]
-fn ui() {
-    dylint_testing::ui_test(
-        env!("CARGO_PKG_NAME"),
-        &std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("ui"),
-    );
-}
