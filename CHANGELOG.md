b6d7885ae7af0db372fb6ae262b0c3b910b245d6
aon <21188659+aon@users.noreply.github.com>
2023-09-12 14:16:50 -0300

    Add changelog and release CI

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## v0.1.1 (2023-06-30)
+
+- Update detector status
+
+## v0.1.0 (2023-06-30)
+
+We're excited to announce the initial release of Scout! This release lays the groundwork for smart contract developers and auditors, to efficiently identify common security issues and deviations from best practices within their ink! smart contracts.
---
+++ scripts/publish-to-crates-io.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+import subprocess
+import tempfile
+import time
+
+def get_package_name(file_path):
+    with open(file_path, "r") as f:
+        for line in f:
+            if line.startswith("name"):
+                return line.split("=")[1].strip().strip('"')
+    raise Exception("Could not find package name in Cargo.toml")
+
+def get_package_version(file_path):
+    with open(file_path, "r") as f:
+        for line in f:
+            if line.startswith("version"):
+                return line.split("=")[1].strip().strip('"')
+    raise Exception("Could not find package version in Cargo.toml")
+
+def is_package_published(package_name, package_version):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        subprocess.call(["cargo", "init"], cwd=tmpdirname)
+        with open(Path(tmpdirname) / "Cargo.toml", "a") as f:
+            f.write(f'{package_name} = "{package_version}"')
+            print(f"Checking if {package_name} {package_version} is published...")
+            try:
+                subprocess.check_output(["cargo", "check"], cwd=tmpdirname)
+                return True
+            except subprocess.CalledProcessError:
+                return False
+            except:
+                raise
+
+def publish_package(package_path):
+    subprocess.call(["cargo", "publish"], cwd=package_path)
+
+if __name__ == "__main__":
+    ROOT_PATH = Path(__file__).parent.parent
+    CARGO_SCOUT_AUDIT_PATH = ROOT_PATH / "apps" / "cargo-scout-audit"
+    SCOUT_AUDIT_INTERNAL_PATH = ROOT_PATH / "scout-audit-internal"
+
+    packages_paths = [SCOUT_AUDIT_INTERNAL_PATH, CARGO_SCOUT_AUDIT_PATH]
+
+    for path in packages_paths:
+        package_name = get_package_name(path / "Cargo.toml")
+        package_version = get_package_version(path / "Cargo.toml")
+        print(f"Publishing {package_name} {package_version}...")
+
+        if not is_package_published(package_name, package_version):
+            publish_package(path)
+            while not is_package_published(package_name, package_version):
+                print(f"{package_name} {package_version} is not published yet, waiting 10 seconds...")
+                time.sleep(10)
+            print(f"{package_name} {package_version} is published")
+        else:
+            print(f"{package_name} {package_version} is already published")
