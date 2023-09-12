c2f027c7b9dd13dd6133f5ea18d8bed7ff7fc940
Facundo Lerena <faculerena@gmail.com>
2023-09-12 13:57:31 -0300

    Fixed output

--- apps/cargo-scout-audit/src/startup.rs
+++ apps/cargo-scout-audit/src/startup.rs
@@ -162,7 +162,9 @@ fn run_dylint(detectors_paths: Vec<PathBuf>, opts: Scout) -> anyhow::Result<()> 
         if path.is_dir() {
             panic!("The output path can't be a directory.");
         }
+        options.pipe_stderr = Some(stderr_temp_file.path().to_str().unwrap().to_string());
     }
+
     if opts.output_format != OutputFormat::Text {
         options.pipe_stderr = Some(stderr_temp_file.path().to_str().unwrap().to_string());
     }
