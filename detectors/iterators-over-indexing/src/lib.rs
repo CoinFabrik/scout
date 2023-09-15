d4023ac9f21274505f461ca91037e85dfc736395
Agustín Losiggio <arlosiggio@gmail.com>
2023-09-15 14:09:02 -0300

    fix

--- detectors/iterators-over-indexing/src/lib.rs
+++ detectors/iterators-over-indexing/src/lib.rs
@@ -4,14 +4,15 @@ extern crate rustc_ast;
 extern crate rustc_hir;
 extern crate rustc_span;
 
-use clippy_utils::diagnostics::span_lint_and_help;
 use rustc_ast::LitKind;
+use rustc_hir::def_id::LocalDefId;
 use rustc_hir::{
     def::Res,
     intravisit::{walk_expr, FnKind, Visitor},
     Expr, ExprKind, HirId, LangItem, LoopSource, MatchSource, PatKind, QPath, StmtKind,
 };
 use rustc_lint::LateLintPass;
+use rustc_span::Span;
 use scout_audit_internal::Detector;
 
 dylint_linting::declare_late_lint! {
@@ -49,50 +50,46 @@ impl<'tcx> Visitor<'tcx> for ForLoopVisitor {
             let ExprKind::Call(func, args) = match_expr.kind &&
             let ExprKind::Path(qpath) = &func.kind &&
             let QPath::LangItem(item, _span, _id) = qpath &&
-            item == &LangItem::IntoIterIntoIter {
+            item == &LangItem::IntoIterIntoIter &&
+            args.first().is_some() &&
+            let ExprKind::Struct(qpath, fields, _) = args.first().unwrap().kind &&
+            let QPath::LangItem(langitem, _span, _id) = qpath &&
+            (
+                LangItem::Range == *langitem ||
+                LangItem::RangeInclusiveStruct == *langitem ||
+                LangItem::RangeInclusiveNew == *langitem
+            ) &&
+            fields.last().is_some() &&
+            let ExprKind::Lit(lit) = &fields.last().unwrap().expr.kind &&
+            let LitKind::Int(_v, _typ) = lit.node &&
+            arms.first().is_some() &&
+            let ExprKind::Loop(block, _, loopsource, _) = arms.first().unwrap().body.kind &&
+            LoopSource::ForLoop == loopsource &&
+            block.stmts.first().is_some() &&
+            let StmtKind::Expr(stmtexpr) = block.stmts.first().unwrap().kind &&
+            let ExprKind::Match(_match_expr, some_none_arms, match_source) = stmtexpr.kind &&
+            MatchSource::ForLoopDesugar == match_source {
 
-            if args.first().is_some() &&
-                let ExprKind::Struct(qpath, fields, _) = args.first().unwrap().kind &&
-                let QPath::LangItem(langitem, _span, _id) = qpath &&
-                (
-                    LangItem::Range == *langitem ||
-                    LangItem::RangeInclusiveStruct == *langitem ||
-                    LangItem::RangeInclusiveNew == *langitem
-                ) &&
-                fields.last().is_some() &&
-                let ExprKind::Lit(lit) = &fields.last().unwrap().expr.kind &&
-                let LitKind::Int(_v, _typ) = lit.node &&
-                arms.first().is_some() &&
-                let ExprKind::Loop(block, _, loopsource, _) = arms.first().unwrap().body.kind &&
-                LoopSource::ForLoop == loopsource &&
-                block.stmts.first().is_some() &&
-                let StmtKind::Expr(stmtexpr) = block.stmts.first().unwrap().kind &&
-                let ExprKind::Match(_match_expr, some_none_arms, match_source) = stmtexpr.kind &&
-                MatchSource::ForLoopDesugar == match_source
-                {
+            let mut visitor = VectorAccessVisitor {
+                has_vector_access: false,
+                index_id: expr.hir_id,
+            };
+            for arm in some_none_arms {
+                if let PatKind::Struct(qpath, pats, _) = &arm.pat.kind &&
+                    let QPath::LangItem(item_type, _, _) = qpath &&
+                    LangItem::OptionSome == *item_type &&
+                    pats.last().is_some() {
 
-                let mut visitor = VectorAccessVisitor {
-                    has_vector_access: false,
-                    index_id: expr.hir_id,
-                };
-                for arm in some_none_arms {
-                    if let PatKind::Struct(qpath, pats, _) = &arm.pat.kind &&
-                        let QPath::LangItem(item_type, _, _) = qpath &&
-                        LangItem::OptionSome == *item_type &&
-                        pats.last().is_some() {
-
-                        if let PatKind::Binding(_, hir_id, _ident, _) = pats.last().unwrap().pat.kind {
-                            visitor.index_id = hir_id;
-                            walk_expr(&mut visitor, arm.body);
-                        }
+                    if let PatKind::Binding(_, hir_id, _ident, _) = pats.last().unwrap().pat.kind {
+                        visitor.index_id = hir_id;
+                        walk_expr(&mut visitor, arm.body);
                     }
-                }
-
-                if visitor.has_vector_access {
-                    self.span_constant.push(expr.span);
                 }
             }
 
+            if visitor.has_vector_access {
+                self.span_constant.push(expr.span);
+            }
         }
         walk_expr(self, expr);
     }
