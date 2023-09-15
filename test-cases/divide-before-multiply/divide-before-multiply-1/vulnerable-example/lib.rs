7c02262dd77bd32b3f22ef524a0e5f14ef841e4f
Agustín Losiggio <arlosiggio@gmail.com>
2023-09-15 11:18:26 -0300

    Improve to handle checked_mul, checked_div and saturating_mul operations

--- test-cases/divide-before-multiply/divide-before-multiply-1/vulnerable-example/lib.rs
+++ test-cases/divide-before-multiply/divide-before-multiply-1/vulnerable-example/lib.rs
@@ -18,6 +18,18 @@ mod divide_before_multiply {
         pub fn split_profit(&self, percentage: u64, total_profit: u64) -> u64 {
             (percentage / 100) * total_profit
         }
+
+        // same as split_profit but using checked functions
+        #[ink(message)]
+        pub fn checked_split_profit(&self, percentage: u64, total_profit: u64) -> Option<u64> {
+            percentage.checked_div(100)?.checked_mul(total_profit)
+        }
+
+        // same as split_profit but using both normal operations and checked functions
+        #[ink(message)]
+        pub fn hybrid_split_profit(&self, percentage: u64, total_profit: u64) -> Option<u64> {
+            Some(percentage.checked_div(100)? * total_profit)
+        }
     }
 
     impl Default for DivideBeforeMultiply {
