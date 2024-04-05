11b7a41b72c754bf3eee8ea0d18139ffe9e45c02
Facundo Lerena <faculerena@gmail.com>
2024-04-05 15:18:39 -0300

    Updated detector

--- detectors/avoid-autokey-upgradable/src/lib.rs
+++ detectors/avoid-autokey-upgradable/src/lib.rs
@@ -1,9 +1,12 @@
 #![feature(rustc_private)]
 #![warn(unused_extern_crates)]
 #![feature(let_chains)]
+extern crate rustc_error_messages;
 extern crate rustc_hir;
 extern crate rustc_span;
 
+use itertools::Itertools;
+use rustc_error_messages::MultiSpan;
 use rustc_hir::GenericArg;
 use rustc_hir::{
     intravisit::{walk_expr, Visitor},
@@ -12,7 +15,6 @@ use rustc_hir::{
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_span::Span;
 use scout_audit_clippy_utils::diagnostics::span_lint_and_help;
-//use scout_audit_internal::{DetectorImpl, InkDetector as Detector};
 
 dylint_linting::impl_late_lint! {
     pub AVOID_AUTOKEY_UPGRADABLE,
@@ -50,27 +52,58 @@ impl<'tcx> LateLintPass<'tcx> for AvoidAutokeyUpgradable {
     }
 
     fn check_field_def(&mut self, cx: &LateContext<'tcx>, field: &'tcx rustc_hir::FieldDef<'tcx>) {
+        if self.lazy_value_has_manual_key(cx, field) {
+            self.lazy_fields.push(field.span);
+        }
+    }
+}
+
+impl<'tcx> AvoidAutokeyUpgradable {
+    fn lazy_value_has_manual_key(
+        &mut self,
+        cx: &LateContext<'_>,
+        field: &'tcx rustc_hir::FieldDef<'tcx>,
+    ) -> bool {
+        //check if the type is lazy
         if let TyKind::Path(QPath::Resolved(Some(ty), _)) = field.ty.kind
             && let TyKind::Path(QPath::Resolved(None, p)) = ty.kind
-            && cx
+            && let def_path = cx
                 .get_def_path(p.res.def_id())
                 .iter()
                 .map(|x| x.to_string())
                 .reduce(|a, b| a + "::" + &b)
                 .unwrap()
-                == "ink_storage::lazy::Lazy"
+            && (is_lazy_type(&def_path))
         {
-            if let Some(gargs) = p.segments[0].args
-                && gargs.args.len() > 1
-                && let GenericArg::Type(ty) = gargs.args[1]
-                && let TyKind::Path(QPath::Resolved(None, p)) = ty.kind
-                && p.segments[0].ident.to_string().contains("ManualKey")
-            {
-            } else {
-                self.lazy_fields.push(field.span);
-            }
+            match p.segments[0].args {
+                //check if the type has generic arg ManualKey<...>
+                Some(gargs) => gargs.args.iter().any(|arg| {
+                    if let GenericArg::Type(ty) = arg
+                        && let TyKind::Path(QPath::Resolved(None, p)) = ty.kind
+                    {
+                        return p
+                            .segments
+                            .iter()
+                            .any(|x| x.ident.name.to_string() != MANUAL_KEY);
+                    };
+                    false
+                }),
+                None => false,
+            };
+            return true;
         }
+        false
     }
+}
+
+const LAZY_TYPE: &str = "ink_storage::lazy::Lazy";
+const MAPPING_TYPE: &str = "ink_storage::lazy::mapping::Mapping";
+const INK_VEC_STORAGE_TYPE: &str = "ink_storage::lazy::vec::StorageVec";
+const SET_CODE_HASH_METHOD: &str = "set_code_hash";
+const MANUAL_KEY: &str = "ManualKey";
+
+fn is_lazy_type(def_path: &str) -> bool {
+    def_path == LAZY_TYPE || def_path == MAPPING_TYPE || def_path == INK_VEC_STORAGE_TYPE
 }
 
 struct AvoidAutokeyUpgradableVisitor<'tcx, 'tcx_ref> {
@@ -81,14 +114,29 @@ struct AvoidAutokeyUpgradableVisitor<'tcx, 'tcx_ref> {
 impl<'tcx> Visitor<'tcx> for AvoidAutokeyUpgradableVisitor<'tcx, '_> {
     fn visit_expr(&mut self, expr: &'tcx Expr<'tcx>) {
         if let Some(v) = expr.method_ident()
-            && v.name.to_string() == "set_code_hash".to_string()
+            && v.name.to_string() == SET_CODE_HASH_METHOD
         {
-            self.lazy_fields.iter().for_each(
+            self.lazy_fields.iter().dedup().for_each(
                 |lazy_field| {
+                    let mut spans : MultiSpan = MultiSpan::from_spans(vec![
+                        *lazy_field,
+                        expr.span,
+                    ]);
+
+                    spans.push_span_label(
+                        *lazy_field,
+                        "This `Lazy` field doesn't have `ManualKey<...>`",
+                    );
+
+                    spans.push_span_label(
+                        expr.span,
+                        "This makes the contract upgradable",
+                    );
+
                     span_lint_and_help(
                         self.cx,
                         AVOID_AUTOKEY_UPGRADABLE,
-                        lazy_field.to(expr.span),
+                        spans,
                         "Avoid using `Lazy` fields without `ManualKey` in upgradable contracts",
                         Some(*lazy_field),
                         &format!(
@@ -103,12 +151,4 @@ impl<'tcx> Visitor<'tcx> for AvoidAutokeyUpgradableVisitor<'tcx, '_> {
         }
         walk_expr(self, expr)
     }
-}
-
-fn foo(x: i32) {
-    let a = x;
-    let b = a;
-    let c = b;
-
-    c.checked_abs();
 }
