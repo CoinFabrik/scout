273073ef2537cf61b480d6d697e04070953f6be9
Camila Gallo <camilagallo1700@gmail.com>
2024-04-25 08:50:20 -0300

    fixed avoid unsafe block

--- detectors/avoid-unsafe-block/src/lib.rs
+++ detectors/avoid-unsafe-block/src/lib.rs
@@ -1,70 +1,36 @@
 #![feature(rustc_private)]
-#![warn(unused_extern_crates)]
 #![feature(let_chains)]
-extern crate rustc_hir;
+extern crate rustc_ast;
 extern crate rustc_span;
 
-use std::vec::Vec;
+use rustc_ast::{BlockCheckMode, Expr, ExprKind, UnsafeSource};
+use rustc_lint::{EarlyContext, EarlyLintPass};
+use scout_audit_clippy_utils::diagnostics::span_lint;
 
-use rustc_hir::{
-    intravisit::{walk_expr, Visitor},
-    BlockCheckMode, Expr, ExprKind, UnsafeSource,
-};
-use rustc_lint::{LateContext, LateLintPass};
-use rustc_span::Span;
-use scout_audit_clippy_utils::diagnostics;
-
-const LINT_MESSAGE: &str = "Avoid using unsafe blocks as it may lead to undefined behavior.";
-dylint_linting::impl_late_lint! {
+const LINT_MESSAGE: &str = "Assert causes panic. Instead, return a proper error.";
+dylint_linting::impl_pre_expansion_lint! {
     pub AVOID_UNSAFE_BLOCK,
     Warn,
     LINT_MESSAGE,
     AvoidUnsafeBlock::default(),
     {
-        name: "Avoid unsafe block",
-        long_message: "The unsafe block is used to bypass Rust's safety checks. It is recommended to avoid using unsafe blocks as much as possible, and to use them only when necessary.    ",
-        severity: "Enhancement",
-        help: "https://coinfabrik.github.io/scout/docs/vulnerabilities/avoid-unsafe-block",
-        vulnerability_class: "Best practices",
+        name: "Unprotected Mapping Operation",
+        long_message: "Modifying mappings with an arbitrary key given by the user could lead to unintented modifications of critical data, modifying data belonging to other users, causing denial of service, unathorized access, and other potential issues.    ",
+        severity: "Critical",
+        help: "https://coinfabrik.github.io/scout/docs/vulnerabilities/unprotected-mapping-operation",
+        vulnerability_class: "Validations and error handling",
     }
 }
 
 #[derive(Default)]
-struct AvoidUnsafeBlock {}
+pub struct AvoidUnsafeBlock {}
 
-impl<'tcx> LateLintPass<'tcx> for AvoidUnsafeBlock {
-    fn check_fn(
-        &mut self,
-        cx: &LateContext<'tcx>,
-        _: rustc_hir::intravisit::FnKind<'tcx>,
-        _: &'tcx rustc_hir::FnDecl<'tcx>,
-        body: &'tcx rustc_hir::Body<'tcx>,
-        _: rustc_span::Span,
-        _: rustc_span::def_id::LocalDefId,
-    ) {
-        let mut unsafety = AvoidUnsafeBlockVisitor { span: Vec::new() };
-
-        walk_expr(&mut unsafety, body.value);
-
-        unsafety
-            .span
-            .iter()
-            .for_each(|sp| diagnostics::span_lint(cx, AVOID_UNSAFE_BLOCK, *sp, LINT_MESSAGE));
-    }
-}
-
-struct AvoidUnsafeBlockVisitor {
-    span: Vec<Span>,
-}
-
-impl<'tcx> Visitor<'tcx> for AvoidUnsafeBlockVisitor {
-    fn visit_expr(&mut self, expr: &'tcx Expr<'tcx>) {
-        if let ExprKind::Block(block, _) = expr.kind
-            && block.rules == BlockCheckMode::UnsafeBlock(UnsafeSource::UserProvided)
-            && block.span.in_macro_expansion_with_collapse_debuginfo()
+impl EarlyLintPass for AvoidUnsafeBlock {
+    fn check_expr(&mut self, cx: &EarlyContext, expr: &Expr) {
+        if let ExprKind::Block(block, ..) = &expr.kind
+            && block.rules == BlockCheckMode::Unsafe(UnsafeSource::UserProvided)
         {
-            self.span.push(expr.span);
+            span_lint(cx, AVOID_UNSAFE_BLOCK, expr.span, LINT_MESSAGE)
         }
-        walk_expr(self, expr)
     }
 }
