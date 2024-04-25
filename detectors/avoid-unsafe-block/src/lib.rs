b029cc2228c8fbf4cd9ec7c501c01a11cdd3f0f7
Camila Gallo <camilagallo1700@gmail.com>
2024-04-25 08:55:55 -0300

    fiex typos

--- detectors/avoid-unsafe-block/src/lib.rs
+++ detectors/avoid-unsafe-block/src/lib.rs
@@ -7,18 +7,18 @@ use rustc_ast::{BlockCheckMode, Expr, ExprKind, UnsafeSource};
 use rustc_lint::{EarlyContext, EarlyLintPass};
 use scout_audit_clippy_utils::diagnostics::span_lint;
 
-const LINT_MESSAGE: &str = "Assert causes panic. Instead, return a proper error.";
+const LINT_MESSAGE: &str = "Avoid using unsafe blocks as it may lead to undefined behavior.";
 dylint_linting::impl_pre_expansion_lint! {
     pub AVOID_UNSAFE_BLOCK,
     Warn,
     LINT_MESSAGE,
     AvoidUnsafeBlock::default(),
     {
-        name: "Unprotected Mapping Operation",
-        long_message: "Modifying mappings with an arbitrary key given by the user could lead to unintented modifications of critical data, modifying data belonging to other users, causing denial of service, unathorized access, and other potential issues.    ",
-        severity: "Critical",
-        help: "https://coinfabrik.github.io/scout/docs/vulnerabilities/unprotected-mapping-operation",
-        vulnerability_class: "Validations and error handling",
+        name: "Avoid unsafe block",
+        long_message: "The unsafe block is used to bypass Rust's safety checks. It is recommended to avoid using unsafe blocks as much as possible, and to use them only when necessary.",
+        severity: "Enhancement",
+        help: "https://coinfabrik.github.io/scout/docs/vulnerabilities/avoid-unsafe-block",
+        vulnerability_class: "Best practices",
     }
 }
 
